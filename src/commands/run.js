dcaf862163291b8af51ae91748fa5ffb7c0133a8
JerryC <huangpeijie@youzan.com>
2019-05-23 15:39:27 +0800

    feat: auto fill RUN_ENV in run command

--- src/commands/run.js
+++ src/commands/run.js
@@ -7,5 +7,6 @@ exports.handler = function handler(argv) {
     const gulpPath = path.join(process.cwd(), './node_modules/.bin/gulp');
     childProcess.spawn(`${gulpPath}`, [argv.env], {
         stdio: [process.stdin, process.stdout, process.stderr],
+        env: Object.assign({ RUN_ENV: argv.env }, process.env),
     });
 };
